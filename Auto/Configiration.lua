a8909d37c68b02879040fbaa5e2348fbeebaae32
AX <pornnsfwsafe@gmail.com>
2026-09-11 07:13:17 -0700

    Update Configiration.lua

--- Auto/Configiration.lua
+++ Auto/Configiration.lua
@@ -133,7 +133,7 @@ return {
     },
 
     -- Standard modes do not have a trial title, so fallback requirements stay separate.
-  local FallbackConfigs = {
+  FallbackConfigs = {
     ["Easy"] = {
         Level = 175,
         Towers = {"Gatling Gun", "Trapper", "Medic", "DJ Booth", "Mercenary Base"},
