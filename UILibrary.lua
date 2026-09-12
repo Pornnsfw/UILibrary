d373e7d7f54c3011ee6ffbf90a5fa072e455ecbf
AX <pornnsfwsafe@gmail.com>
2026-09-11 22:02:49 -0700

    Update UILibrary.lua

--- UILibrary.lua
+++ UILibrary.lua
@@ -1,10 +1,5 @@
 local Globals = getgenv()
 
-if shared.TDSTable then
-    return shared.TDSTable
-end
-
-
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 local GuiService = game:GetService("GuiService")
